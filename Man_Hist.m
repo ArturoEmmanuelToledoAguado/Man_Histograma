5376b32556e299852b454d558b0dfe85ad9e62cc
unknown <atoledoa1300@alumno.ipn.mx>
2022-10-19 17:49:13 -0500

    Enrada de datos

--- Man_Hist.m
+++ Man_Hist.m
@@ -1,3 +1,10 @@
 clc; 
 clear all;
 close all;
+
+%Abrir imagen
+prompt={"Ingrese el nombre de la imagen con el .jpg"};
+title='Imagen';
+dims=[1 35];
+definput={'IMG/'};
+answer=inputdlg(prompt,title,dims,definput)
