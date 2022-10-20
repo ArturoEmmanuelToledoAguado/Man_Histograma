717be78f99dea00928a545440ae4517d4081b3c3
ArturoEmmanuelToledoAguado <atoledoa1300@alumno.ipn.mx>
2022-10-19 21:58:04 -0500

    Iteracion para mostrar todas las IMG, sigue Doc

--- Man_Hist.m
+++ Man_Hist.m
@@ -2,51 +2,57 @@ clc;
 clear all;
 close all;
 
-%Interfaz para pedir la imagen
-prompt="Ingrese el nombre de la imagen";
-t='Imagen';
-dims=[1 50];
-definput={'IMG/'};
-answer=inputdlg(prompt,t,dims,definput);
+% %Interfaz para pedir la imagen
+% prompt="Ingrese el nombre de la imagen";
+% t='Imagen';
+% dims=[1 50];
+% definput={'IMG/'};
+% answer=inputdlg(prompt,t,dims,definput);
+% 
+% %Abrir imagen
+% img=imread(string(answer)+'.jpg');
 
-%Abrir imagen
-img=imread(string(answer)+'.jpg');
+ruta = {'IMG/BN1','IMG/BN2','IMG/BN3','IMG/Col1','IMG/Col2','IMG/Col3'};
 
-%Mostramos original y su histograma
-figure()
-subplot(1,2,1)
-imshow(img)
-title('Original')
-subplot(1,2,2)
-imhist(img)
-title('Histograma')
+for k=1:6
+    %Abrir imagen
+    img=imread(string(ruta{k})+'.jpg');
+    %Mostramos original y su histograma
+    figure(k)
+    subplot(1,2,1)
+    imshow(img)
+    title('Original')
+    subplot(1,2,2)
+    imhist(img)
+    title('Histograma')
 
-%Histograma con gamma predeterminado (1)
-imgjs=imadjust(img,[.16 .83],[.0833 .9166]);%[42.5 <-> 212.5]/6, [21.25 <-> 233.75]/12
+    %Histograma con gamma predeterminado (1)
+    imgjs=imadjust(img,[.16 .83],[.0833 .9166]);%[42.5 <-> 212.5]/6, [21.25 <-> 233.75]/12
 
-%Histograma con gamma <1 (más brillante)
-imgjsb=imadjust(img,[.01 .85],[.08 .91],.6);%[2.55 <-> 216.75] , [21.25 <-> 233.75]/12
+    %Histograma con gamma <1 (más brillante)
+    imgjsb=imadjust(img,[.01 .85],[.08 .91],.6);%[2.55 <-> 216.75] , [21.25 <-> 233.75]/12
 
-%Histograma con gamma >1 (más oscuro)
-imgjso=imadjust(img,[.12 .87],[.083 .916],1.4);%[32.875 <-> 223.125]/8 , [21.25 <-> 233.75]/12
+    %Histograma con gamma >1 (más oscuro)
+    imgjso=imadjust(img,[.12 .87],[.083 .916],1.4);%[32.875 <-> 223.125]/8 , [21.25 <-> 233.75]/12
 
-%Mostramos resultados
-figure()
-subplot(3,2,1)
-imshow(imgjs)
-title('\gamma = 1')
-subplot(3,2,2)
-imhist(imgjs)
-title('Histograma \gamma = 1')
-subplot(3,2,3)
-imshow(imgjsb)
-title('\gamma = .6')
-subplot(3,2,4)
-imhist(imgjsb)
-title("Histograma \gamma = .6")
-subplot(3,2,5)
-imshow(imgjso)
-title("\gamma = 1.4")
-subplot(3,2,6)
-imhist(imgjso)
-title("Histograma \gamma = 1.4")
+    %Mostramos resultados
+    figure(k+6)
+    subplot(3,2,1)
+    imshow(imgjs)
+    title('\gamma = 1')
+    subplot(3,2,2)
+    imhist(imgjs)
+    title('Histograma \gamma = 1')
+    subplot(3,2,3)
+    imshow(imgjsb)
+    title('\gamma = .6')
+    subplot(3,2,4)
+    imhist(imgjsb)
+    title("Histograma \gamma = .6")
+    subplot(3,2,5)
+    imshow(imgjso)
+    title("\gamma = 1.4")
+    subplot(3,2,6)
+    imhist(imgjso)
+    title("Histograma \gamma = 1.4")
+end
