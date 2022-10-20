32c4983741a8fb6b8cccfaab67a44327c88c4dd1
ArturoEmmanuelToledoAguado <atoledoa1300@alumno.ipn.mx>
2022-10-19 20:42:16 -0500

    Histograma original

--- Man_Hist.m
+++ Man_Hist.m
@@ -2,9 +2,21 @@ clc;
 clear all;
 close all;
 
-%Abrir imagen
-prompt={"Ingrese el nombre de la imagen con el .jpg"};
-title='Imagen';
-dims=[1 35];
+%Interfaz para pedir la imagen
+prompt="Ingrese el nombre de la imagen";
+t='Imagen';
+dims=[1 50];
 definput={'IMG/'};
-answer=inputdlg(prompt,title,dims,definput)
+answer=inputdlg(prompt,t,dims,definput);
+
+%Abrir imagen
+img=imread(string(answer)+'.jpg');
+
+%Mostramos original y su histograma
+figure()
+subplot(1,2,1)
+imshow(img)
+title('Original')
+subplot(1,2,2)
+imhist(img)
+title('Histograma')
