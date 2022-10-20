8bf7049019d0628bd6d5c7f06b7035e09f0cf2a2
ArturoEmmanuelToledoAguado <atoledoa1300@alumno.ipn.mx>
2022-10-19 21:47:13 -0500

    Recalibracion de gammas y salidas

--- Man_Hist.m
+++ Man_Hist.m
@@ -22,13 +22,13 @@ imhist(img)
 title('Histograma')
 
 %Histograma con gamma predeterminado (1)
-imgjs=imadjust(img,[.01 .85],[]);
+imgjs=imadjust(img,[.16 .83],[.0833 .9166]);%[42.5 <-> 212.5]/6, [21.25 <-> 233.75]/12
 
 %Histograma con gamma <1 (más brillante)
-imgjsb=imadjust(img,[.01 .85],[],.5);
+imgjsb=imadjust(img,[.01 .85],[.08 .91],.6);%[2.55 <-> 216.75] , [21.25 <-> 233.75]/12
 
 %Histograma con gamma >1 (más oscuro)
-imgjso=imadjust(img,[.01 .85],[],1.3);
+imgjso=imadjust(img,[.12 .87],[.083 .916],1.4);%[32.875 <-> 223.125]/8 , [21.25 <-> 233.75]/12
 
 %Mostramos resultados
 figure()
@@ -40,12 +40,13 @@ imhist(imgjs)
 title('Histograma \gamma = 1')
 subplot(3,2,3)
 imshow(imgjsb)
-title('\gamma = .5')
+title('\gamma = .6')
 subplot(3,2,4)
 imhist(imgjsb)
-title("Histograma \gamma =.5")
+title("Histograma \gamma = .6")
 subplot(3,2,5)
 imshow(imgjso)
-title("\gamma = 1.3")
+title("\gamma = 1.4")
 subplot(3,2,6)
 imhist(imgjso)
+title("Histograma \gamma = 1.4")
