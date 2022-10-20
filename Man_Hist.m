3695a362129610fa5a5fa41f9ec04481c170b3c1
ArturoEmmanuelToledoAguado <atoledoa1300@alumno.ipn.mx>
2022-10-19 21:16:41 -0500

    Histogramas con Gammas

--- Man_Hist.m
+++ Man_Hist.m
@@ -20,3 +20,32 @@ title('Original')
 subplot(1,2,2)
 imhist(img)
 title('Histograma')
+
+%Histograma con gamma predeterminado (1)
+imgjs=imadjust(img,[.01 .85],[]);
+
+%Histograma con gamma <1 (más brillante)
+imgjsb=imadjust(img,[.01 .85],[],.5);
+
+%Histograma con gamma >1 (más oscuro)
+imgjso=imadjust(img,[.01 .85],[],1.3);
+
+%Mostramos resultados
+figure()
+subplot(3,2,1)
+imshow(imgjs)
+title('\gamma = 1')
+subplot(3,2,2)
+imhist(imgjs)
+title('Histograma \gamma = 1')
+subplot(3,2,3)
+imshow(imgjsb)
+title('\gamma = .5')
+subplot(3,2,4)
+imhist(imgjsb)
+title("Histograma \gamma =.5")
+subplot(3,2,5)
+imshow(imgjso)
+title("\gamma = 1.3")
+subplot(3,2,6)
+imhist(imgjso)
